4ec2fc6930d40059c04422fd25fd9cb9d98b80fe
linzengrui <lnzgrv017@163.com>
2018-01-30 18:12:09 +0800

    webmagic example

---
+++ bayberry-acquistionplatform/bayberry-acquistionplatform-core/src/main/java/cn/zerry/bayberry/acquistionplatform/core/common/processor/GithubRepo.java
@@ -0,0 +1,36 @@
+package cn.terry.acquistionplatform.common.processor;
+
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.model.ConsolePageModelPipeline;
+import us.codecraft.webmagic.model.OOSpider;
+import us.codecraft.webmagic.model.annotation.ExtractBy;
+import us.codecraft.webmagic.model.annotation.ExtractByUrl;
+import us.codecraft.webmagic.model.annotation.HelpUrl;
+import us.codecraft.webmagic.model.annotation.TargetUrl;
+import us.codecraft.webmagic.pipeline.JsonFilePipeline;
+
+/**
+ * @author linzengrui
+ * @Description 基于注解实现GithubRepoPageProcessor类型功能
+ * @date 2018年01月20日 22:08
+ */
+@TargetUrl("https://github.com/\\w+/\\w+")
+@HelpUrl("https://github.com/\\w+")
+public class GithubRepo {
+    @ExtractBy(value = "//h1[@class='entry-title public']/strong/a/text()", notNull = true)
+    private String name;
+
+    @ExtractByUrl("https://github\\.com/(\\w+)/.*")
+    private String author;
+
+    @ExtractBy("//div[@id='readme']/tidyText()")
+    private String readme;
+
+    public static void main(String[] args) {
+        OOSpider.create(Site.me().setSleepTime(1000), new ConsolePageModelPipeline(), GithubRepo.class)
+                .addUrl("https://github.com/code4craft")
+                .addPipeline(new JsonFilePipeline("D:\\webmagic\\"))
+                .thread(5)
+                .run();
+    }
+}
