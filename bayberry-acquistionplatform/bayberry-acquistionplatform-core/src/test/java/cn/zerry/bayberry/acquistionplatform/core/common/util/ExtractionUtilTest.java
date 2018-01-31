b420049b7e9e2b99003f8cf9b24fc5f511566e6a
linzengrui <lnzgrv017@163.com>
2018-01-31 16:55:51 +0800

    extraction

---
+++ bayberry-acquistionplatform/bayberry-acquistionplatform-core/src/test/java/cn/zerry/bayberry/acquistionplatform/core/common/util/ExtractionUtilTest.java
@@ -0,0 +1,21 @@
+package cn.zerry.bayberry.acquistionplatform.core.common.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author linzengrui
+ * @Description TODO
+ * @date 2018年01月31日 11:30
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class ExtractionUtilTest {
+    @Test
+    public void extractByEnds() throws Exception{
+
+    }
+
+}
