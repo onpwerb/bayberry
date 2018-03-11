2be97822f881b4ef9881733ee28b8d1da1081fcf
linzengrui <lnzgrv017@163.com>
2018-03-11 23:18:56 +0800

    netty server client

---
+++ bayberry-zpush/README.md
@@ -0,0 +1,17 @@
+**project**
+
+
+**netty**
+
+netty is an asynchronous event-driven network application framework.
+
+core components
+
+- Channels 
+- Callbacks 
+- Futures 
+- Events and handlers
+
+ 
+
+
---
+++ bayberry-zpush/src/main/java/cn/zerry/ZPushApplication.java
@@ -0,0 +1,16 @@
+package cn.zerry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author linzengrui
+ * @Description TODO
+ * @date 2018年03月11日 15:51
+ */
+@SpringBootApplication
+public class ZPushApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZPushApplication.class, args);
+    }
+}
