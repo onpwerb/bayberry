079d6a76aa8fdddf46cbb96d3f8123b2ca366ff9
linzengrui <lnzgrv017@163.com>
2018-05-03 09:01:19 +0800

    spring boot security

---
+++ bayberry-cms/src/main/java/cn/zerry/samples/security/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package cn.zerry.samples.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author linzengrui
+ * @Description TODO
+ * @date 2018年05月02日 15:35
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/css/**", "/index").permitAll()
+                .antMatchers("/user/**").hasRole("USER")
+                .and()
+                .formLogin()
+                .loginPage("/login").failureUrl("/login-error");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+
+
+}
